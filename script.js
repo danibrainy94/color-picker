5906e28204120e6fe540baa7c5f27c7253c70e72
danibrainy94 <danibrainy94@gmail.com>
2023-05-20 13:37:51 +0100

    master

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
+const simpleValues = document.querySelector(".simple")
+const hexValues = document.querySelector(".hex")
+
 const colorName = document.querySelector("#color-text");
 const color = document.querySelector("#color-display");
 const button = document.querySelector("#btn");
 const diffColors = ["Amaranth Purple", "Barn red", "Bittersweet", "Flame", "Wenge", "Dark moss green", "Process cyan", "Antique white", "Mint", "Sinopia"];
 
-// hexColors = ["#AB274F", "#7C0902", "#FE6F5E", "#E25822", "#645452", "#4A5D23", "#00B7EB", "#FAEBD7", "#3EB489", "#CB410B",];
 
-function changeColor() {
-    let determinant = Math.floor(Math.random() * 10);
-    color.style.backgroundColor = diffColors[determinant];
 
-    colorName.textContent = diffColors[determinant];
+// selecting the color type 
+
+
+
+const hexColors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+function getRandomNum() {
+    return Math.floor(Math.random() * hexColors.length);
+} 
+
+function getHexColor () {
+
+    if (hexValues == "clicked") {
+        let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+        hexColor += hexColors[getRandomNum()];
+        console.log(hexColor);
+        }
+        colorName.textContent = hexColor;
+        color.style.backgroundColor = hexColor;
+
+    }
+    }
+
+function getSimpleColor() {
+    if (simpleValues == "clicked") {
+        let determinant = Math.floor(Math.random() * 10);
+        color.style.backgroundColor = diffColors[determinant];
+        colorName.textContent = diffColors[determinant];
+    }
 }
 
-button.addEventListener("click", changeColor);
+
+
+button.addEventListener("click", getHexColor);
+button.addEventListener("click", getSimpleColor);
+
+
+
+
