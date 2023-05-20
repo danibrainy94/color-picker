ff1b6bbb43f6409db6c898d2026c0409c7e9f723
danibrainy94 <danibrainy94@gmail.com>
2023-05-20 01:27:20 +0100

    master

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const colorName = document.querySelector("#color-text");
 const color = document.querySelector("#color-display");
 const button = document.querySelector("#btn");
-const diffColors = ["green", "blue", "red", "orange", "white", "brown", "chocolate", "crimson", "magenta", "seagreen"];
+const diffColors = ["Amaranth Purple", "Barn red", "Bittersweet", "Flame", "Wenge", "Dark moss green", "Process cyan", "Antique white", "Mint", "Sinopia"];
+
+// hexColors = ["#AB274F", "#7C0902", "#FE6F5E", "#E25822", "#645452", "#4A5D23", "#00B7EB", "#FAEBD7", "#3EB489", "#CB410B",];
 
 function changeColor() {
     let determinant = Math.floor(Math.random() * 10);
@@ -11,5 +13,3 @@ function changeColor() {
 }
 
 button.addEventListener("click", changeColor);
-
-const hexSelector = 
